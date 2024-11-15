6b07f7729232079d66007deab8ed1d1a0b1c6c6d
Fusion <leemenhin@gmail.com>
2024-11-15 10:43:32 +0800

    Update settings.mk

--- configs/settings.mk
+++ configs/settings.mk
@@ -1,5 +1,5 @@
 KERNELREV="3"
 FSBLVERSION=1.1.0
-PACKAGES="ca-certificates wireless-tools wpasupplicant dhclient curl debian-archive-keyring binutils network-manager u-boot-menu dropbear libpam-systemd libengine-pkcs11-openssl systemd-timesyncd nano usbutils parted systemd-sysv ethtool gnupg u-boot-tools libubootenv-tool"
+PACKAGES="ca-certificates wireless-tools wpasupplicant isc-dhcp-client curl debian-archive-keyring binutils network-manager u-boot-menu dropbear libpam-systemd libengine-pkcs11-openssl systemd-timesyncd nano usbutils parted systemd-sysv ethtool gnupg u-boot-tools libubootenv-tool"
 
 IMAGE_ADDITIONS="usb-gadget"
