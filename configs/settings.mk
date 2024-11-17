6b72671022408340841ad324d486e70c7b88c61b
Fusion <leemenhin@gmail.com>
2024-11-18 05:32:51 +0800

    Update settings.mk

--- configs/settings.mk
+++ configs/settings.mk
@@ -1,5 +1,3 @@
 KERNELREV="3"
 FSBLVERSION=1.1.0
 PACKAGES="ca-certificates libpam-systemd debian-archive-keyring u-boot-menu dropbear systemd-timesyncd nano usbutils parted systemd-sysv u-boot-tools libubootenv-tool"
-
-IMAGE_ADDITIONS=""
