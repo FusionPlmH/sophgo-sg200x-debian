7cc77248d52c19476046a8784ebd49a52b5ce8f0
Fusion <leemenhin@gmail.com>
2024-11-18 12:03:25 +0800

    Update settings.mk

--- configs/settings.mk
+++ configs/settings.mk
@@ -2,4 +2,4 @@ KERNELREV="3"
 FSBLVERSION=1.1.0
 PACKAGES="ca-certificates libpam-systemd debian-archive-keyring u-boot-menu dropbear systemd-timesyncd nano usbutils parted systemd-sysv u-boot-tools libubootenv-tool"
 
-IMAGE_ADDITIONS=“usb-gadget”
+IMAGE_ADDITIONS="usb-gadget"
